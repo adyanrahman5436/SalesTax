3dc6bc89e690fff9512899f7a36d39f2c809302e
adyanrahman5436 <117797159+adyanrahman5436@users.noreply.github.com>
2023-04-17 18:23:48 -0500

    Add files via upload

---
+++ SalesTaxCalculator.cpp
@@ -0,0 +1,51 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+//this program gets the county and state tax on a purchase and combines them to see the total tax
+
+int
+main ()
+{
+    
+    //declaring the variables and assigning the value of what they are
+    
+    double countyTax, stateTax, totalTax, countyTax2, stateTax2;
+    countyTax = 0.02;
+    stateTax = 0.06;
+    double purchase;
+
+    //getting an input for purchase price of an item
+
+    cout << "Purchase Cost = " << endl;
+    cin >> purchase;
+    
+    //calculating the state and county tax based on the assigned value for them
+
+    countyTax2 = (countyTax * purchase);
+    stateTax2 = (stateTax * purchase);
+
+    //giving an output on how much the sales taxes are
+    
+    cout << "State sales tax = $" << stateTax2 << endl;
+    cout << "County sales tax = $" << countyTax2 << endl;
+    
+    //calculation of total tax
+
+    totalTax = stateTax2 + countyTax2;
+    
+    //output for total tax
+
+    cout << "Total sales tax = $" << totalTax << endl;
+
+    return 0;
+}
+
